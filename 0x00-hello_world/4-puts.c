b1ff8ee72e9b9197140ed8620e9c9724a1574986
jonyngash <jonyngash2@gmail.com>
2023-02-17 00:50:32 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
-/**
- * main - Entry point
- * Return: Always 0 (Success)
+/*
+ * main - print a line of code using puts
+ *
+ * Return: 0
  */
-
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
