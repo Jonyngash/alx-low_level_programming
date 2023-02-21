2e0041ab7f7ec5583a1de8dba85866cbf24e3ec3
jonyngash <jonyngash2@gmail.com>
2023-02-20 22:57:08 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("Size of char: %lu byte(s)\n", sizeof(char));
@@ -12,6 +13,5 @@ int main(void)
 	printf("size of a long int: %lu bytes(s)\n", sizeof(long int));
 	printf("size of a long long int: %lu bytes(s)\n", sizeof(long long int));
 	printf("size of a float: %lu bytes(s)\n", sizeof(float));
-
 	return (0);
 }
